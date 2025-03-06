601cfc41f59c321d65a1b05c1509384b702f6c8d
Carlo <you@example.comcarlo.diiorio@si2001.it>
2025-03-06 13:05:18 +0100

    fix dtoPrenotazione

--- src/main/java/com/example/noleggioautobe/dto/DtoPrenotazione.java
+++ src/main/java/com/example/noleggioautobe/dto/DtoPrenotazione.java
@@ -1,11 +1,15 @@
 package com.example.noleggioautobe.dto;
 
 import com.example.noleggioautobe.entities.Prenotazione;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DtoPrenotazione {
 
     private Integer id;
